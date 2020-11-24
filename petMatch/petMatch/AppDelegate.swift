31f32d1b2d0aac58ad31c92941ce4282327e23c3
Anisha Kabir <ahsina.ribak@gmail.com>
2020-11-24 00:46:27 -0800

    set up parse server

--- petMatch/petMatch/AppDelegate.swift
+++ petMatch/petMatch/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "8etf39DZu67UrKJVd6I2K5Mi8VUfIRMGfB4rmVVk"
+                $0.clientKey = "RxHl8RlVaXFvY5PTaW7qLkLdzMXukEXOnwqfmlVV"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
